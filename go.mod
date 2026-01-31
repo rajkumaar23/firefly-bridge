3496d181a6db1940c37ee11578f41adec60cbe75
Rajkumar <rajkumaar2304@icloud.com>
2026-01-31 12:53:09 -0800

    add config parser

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rajkumaar23/firefly-bridge
 
 go 1.25.6
+
+require gopkg.in/yaml.v2 v2.4.0
---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type FireflyConfig struct {
+	Host  string `yaml:"host"`
+	Token string `yaml:"token"`
+}
+
+type Config struct {
+	Firefly FireflyConfig `yaml:"firefly"`
+}
+
+func NewConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %w", err)
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal yaml: %w", err)
+	}
+
+	return &cfg, nil
+}
